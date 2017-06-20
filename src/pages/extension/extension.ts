b819642f6980f9b59c3015f489b542069cab5d3f
Thomas Nghiem <thuongnt@qsoftvietnam.com>
2017-06-20 21:12:35 +0700

    add extension

---
+++ src/pages/extension/extension.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-extension',
+  templateUrl: 'extension.html'
+})
+export class ExtensionPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
